5c128ab14304c9f054ee6b3eed512c9dd74334ff
Kenneth Kwan <khkwan0@gmail.com>
2017-05-19 12:15:32 +0000

    added sessions, db, passport auth

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var passport = require('passport'),
+        LocalStrategy = require('passport-local').Strategy,
+        FacebookStrategy = require('passport-facebook').Strategy;
+var config = require('./config');
 var db = monk(config.mongo.host+':'+config.mongo.port+'/'+config.mongo.db);
 
 var index = require('./routes/index');
 var nunjucks = require('nunjucks');
+var moment = require('moment');
+var sendMail = require('sendmail')()
 
 var app = express();
 nunjucks.configure('views', {
@@ -31,13 +36,157 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-    store: new MongoStore({ url: 'mongodb://'+config.session.store.host+':'+config.session.store.port+'/'+config.session.store.db })
+    secret: config.session.secret,
+    store: new MongoStore({ url: 'mongodb://'+config.session.store.host+':'+config.session.store.port+'/'+config.session.store.db }),
+    saveUninitialized: false,
+    resave: false
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    req.db=db;
+    req.passport = passport;
+	next();
+});
 
 app.use('/', index);
 
+// send mail
+function sendmail(mailOptions) {
+    sendMail(mailOptions, (err, reply) => {
+        if (err) {
+            console.log(err && err.stack);
+        }
+        console.log(reply);
+    });
+}
+passport.use(new FacebookStrategy({
+            clientID: config.facebook.clientID,
+            clientSecret: config.facebook.clientSecret,
+            callbackURL: config.facebook.callbackURL,
+            profileFields: ['id', 'photos', 'emails']
+        },
+        function (successToken, refreshToken, profile, done) {
+            email = profile.emails[0].value;
+			let collection = db.get('users');
+            collection.find({email:email}, (err, user)=> {
+                if (err) {
+                    return done(err);
+                }
+                if (user) {
+                    if (typeof user.profile === 'undefined') {
+                        user.profile = {};
+                    }
+                    if (typeof user.profile.fb === 'undefined') {
+                        user.profile.fb = profile;
+                    }
+                    user.lastlogin = new Date();
+                    collection.update({email:email}, user, { upsert: true}, (err, result)=> {
+                        if (err) {
+                            throw(err);
+                        }
+                        done(null, user);
+                    });
+                } else { 
+                    let datetime = new Date();
+
+                    var new_user = {
+                        'email': email,
+                        'datetime': datetime,
+                        'profile': {fb:profile},
+                        'admin': 0,
+                        'lastlogin': datetime,
+                    }
+                    mailOptions = {
+                        from: config.email.from,
+                        to: 'fillmein@fillmein.com',
+                        subject: 'New user: '+ email,
+                        html: '<div>Date: ' + new_user.datetime + '</div><br /><div><pre>' + new_user.profile + '</pre></div>'
+                    }
+                    sendmail(mailOptions);
+                    collection.insert(new_user, (err, result) => {
+                        if (err) {
+                            throw(err);
+                        }
+                        return done(null, new_user);
+                    });
+                }
+            });
+        }
+));
+
+passport.use('local-login', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+}, (req, email, password, done) => {
+	let collection = db.get('users');
+	collection.find({email:email}, (err, data) => {
+        if (err) {
+            return done(err);
+        }
+        if (!data) {
+            return done(null, false, req.flash('loginMessage', 'No user found'));
+        } else {
+            user = data;
+            user.lastlogin = new Date();
+            collection.update({email:email}, user, { upsert: true }, (err, result)=> {
+				if (err) {
+					console.log(err);
+				}
+			});
+            if (password != user.password) {
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password'));
+            }
+            return done(null, user);
+        }
+    });
+}));
+
+passport.use('local-register', new LocalStrategy({
+                usernameField : 'email',
+                passwordField : 'password',
+                passReqToCallback : true
+            }, function(req, email, password, done) {
+                process.nextTick(function() {
+					let collection = db.get('users');
+                    collection.find({email:email}, (err, user) => {
+                        if (err) {
+                            done(err);
+                        }
+                        if (user) {
+                            return done(null, false, req.flash('signupMessage', 'That email already exists.'));
+                        } else {
+							let datetime = new Date();
+                            var new_user = {
+                                email: email,
+                                password: password,
+                                datetime: datetime,
+                                profile: {
+                                    local: {}
+                                },
+                                admin: 0,
+                                lastlogin: datetime
+                            }
+                            mailOptions = {
+                                from: config.email.from,
+                                to: 'fillmein@fillmein.com',
+                                subject: 'New user: '+ email,
+                                html: '<div>Date: ' + new_user.datetime + '</div><br /><div><pre>' + new_user.profile + '</pre></div>'
+                            }
+                            sendmail(mailOptions);
+                            collection.insert(new_user, (err, result) => {
+								if (err) {
+									console.log(err);
+								}	
+							});
+                            return done(null, new_user);
+                        }
+                    });
+                });
+            })
+);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: config.site.title });
 });
 
+router.get('/auth/facebook', (req, res, next)=> {
+    let passport = req.passport;
+    passport.authenticate('facebook', {scope: 'email'});
+});
+
+router.get('/auth/facebook/callback', (req, res, next) => {
+    let passport = req.passport;
+    passport.authenticate('facebook', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    });
+});
+
+router.post('/auth/local', (req, res, next) => {
+    let passport = req.passport;
+    passport.authenticate('local-login', {
+        successRedirect: '/status',
+        failureRedirect: '/login',
+        failureFlash: true
+    });
+});
+
 module.exports = router;
